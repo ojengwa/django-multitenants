6f98a6e494ec348f26902a20a0711a91762328d2
Bernard Ojengwa <bernardojengwa@gmail.com>
2016-03-16 00:06:12 +0100

    modify gitignore

--- examples/tenant_tutorial/tenant_tutorial/urls_public.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-from tenant_tutorial.views import HomeView
-
-urlpatterns = patterns('',
-                       url(r'^$', HomeView.as_view()),
-                       )
--- examples/tenant_tutorial/tenant_tutorial/urls_tenants.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-from customers.views import TenantView
-
-urlpatterns = patterns('',
-                       url(r'^$', TenantView.as_view()),
-                       )
